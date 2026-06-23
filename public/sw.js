5d619d6d26792c55339d55d0d87113d9d120e7dd
Arya Dwi Putra <aryadptr.developer@gmail.com>
2026-06-23 07:31:49 +0700

    Add PWA manifest, theme-color meta, and service worker

---
+++ public/sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'pos-cache-v1';
+const STATIC_ASSETS = ['/manifest.json'];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cached) => cached || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))))
+    );
+});
